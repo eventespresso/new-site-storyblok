cbe58ed5b9ed943182a2c9b01109d406045dee45
tn3rb <brent@eventespresso.com>
2023-03-11 19:36:42 -0800

    use @import for cdn fonts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,6 @@ const Layout: React.FC<LayoutProps> = ({ children, pageTitle = 'Event Espresso' 
 		<Head>
 			<title>{pageTitle}</title>
 			<link rel='icon' href='/favicon.ico' />
-			<link rel="stylesheet" href="https://fonts.cdnfonts.com/css/groupe" />
 		</Head>
 		<body className="flex flex-col h-80 justify-between font-content antialiased">
 			<Header />
