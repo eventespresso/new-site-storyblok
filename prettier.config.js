6dd46363f919d827d4b47d8efeb2abf1c283cb5a
tn3rb <brent@eventespresso.com>
2023-03-11 19:24:15 -0800

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'auto',
+	endOfLine: 'lf',
+	jsxSingleQuote: true,
+	plugins: [require('prettier-plugin-tailwindcss')],
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+};
