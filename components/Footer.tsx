5641069fdde2bc087339f2519cba6bf49d9d2dbe
tn3rb <brent@eventespresso.com>
2023-03-11 18:40:52 -0800

    fix links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,99 +8,99 @@ const Footer = () => {
 
 				<div className="flex flex-col items-start mt-4 mx-4">
 					<h4 className='mb-2'>Features</h4>
-					<Link href="/features/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Core Features
+					<Link href="/features/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Core Features</a>
 					</Link>
-					<Link href="/add-ons/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Available Add-ons
+					<Link href="/add-ons/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Available Add-ons</a>
 					</Link>
 				</div>
 
 				<div className="flex flex-col items-start mt-4 mx-4">
 					<h4 className='mb-2'>Products</h4>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Pricing
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Pricing</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Use Cases
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Use Cases</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Requirements
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Requirements</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Testimonials
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Testimonials</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Demo Event Espresso
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Demo Event Espresso</a>
 					</Link>
 				</div>
 
 				<div className="flex flex-col items-start mt-4 mx-4">
 					<h4 className='mb-2'>About</h4>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Write For Us
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Write For Us</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						About Event Espresso
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">About Event Espresso</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Common Questions
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Common Questions</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Privacy Notice
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Privacy Notice</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Privacy Policy
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Privacy Policy</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Terms & Conditions
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Terms & Conditions</a>
 					</Link>
 				</div>
 
 				<div className="flex flex-col items-start mt-4 mx-4">
 					<h4 className='mb-2'>Support</h4>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						My Account
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">My Account</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Contact Us
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Contact Us</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Support Resources
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Support Resources</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Support Forums
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Support Forums</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Developer Center
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Developer Center</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Github Profile
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Github Profile</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Tell a Friend
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Tell a Friend</a>
 					</Link>
 				</div>
 
 				<div className="flex flex-col items-start mt-4 mx-4">
 					<h4 className='mb-2'>Resources</h4>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Blog
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Blog</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Guides & Strategies
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Guides & Strategies</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Competitor Comparison
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Competitor Comparison</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Public Roadmap
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Public Roadmap</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Hire a Consultant
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Hire a Consultant</a>
 					</Link>
-					<Link href="/xxxxxxxxxxxxxx/" className="text-sky-500 hover:text-sky-200 mb-2">
-						Change Log
+					<Link href="/xxxxxxxxxxxxxx/">
+						<a className="text-sky-500 hover:text-sky-200 mb-2">Change Log</a>
 					</Link>
 				</div>
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,41 +8,47 @@ const Header: React.FC = () => (
 				<div className="flex flex-col sm:flex-row items-center mb-4 sm:mb-0">
 					<div className="sm:ml-8 flex space-x-5 text-center">
 						<Link href="/">
-							<span className="flex items-center">
+							<a className="flex items-center">
 								<img src="/event-espresso-cup-logo.png"
 									className="logo-image w-16"
 									alt="Event Espresso"
 								/>
 								<span className='ee-wordmark text-white hover:text-slate-800 text-4xl font-black transition duration-200'>event espresso</span>
-							</span>
+							</a>
 						</Link>
 					</div>
 					<div className="sm:ml-8 flex justify-around text-center">
 						<Link href="/pricing">
-							<span className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">Pricing</span>
+							<a className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">
+								Pricing
+							</a>
 						</Link>
 						<Link href="/features">
-							<span className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">Features</span>
+							<a className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">
+								Features
+							</a>
 
 						</Link>
 						<Link href="/use-cases">
-							<span className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">Use Cases</span>
+							<a className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">
+								Use Cases
+							</a>
 
 						</Link>
 						<Link href="/add-ons">
-							<span className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">Add-ons</span>
+							<a className="text-white hover:bg-slate-800 py-0.5 px-2 rounded-sm mx-1 transition duration-200">
+								Add-ons
+							</a>
 
 						</Link>
 					</div>
 				</div>
-				<Link
-					href="/admin"
-					prefetch={false}
-					className="py-1 px-1 rounded text-white font-medium border border-white hover:bg-slate-800 hover:border-slate-800 transition duration-200"
-				>
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-						<path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clipRule="evenodd" />
-					</svg>
+				<Link href="/admin" prefetch={false}>
+					<a className="py-1 px-1 rounded text-white font-medium border border-white hover:bg-slate-800 hover:border-slate-800 transition duration-200">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+							<path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clipRule="evenodd" />
+						</svg>
+					</a>
 				</Link>
 			</div>
 		</div>
