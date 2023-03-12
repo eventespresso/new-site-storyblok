9004705d2f4feef993e9e878fa6d640c8449b65a
tn3rb <brent@eventespresso.com>
2023-03-11 18:55:52 -0800

    use body instead of div

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,21 +9,19 @@ interface LayoutProps {
 	pageTitle?: string
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, pageTitle = 'Event Espresso' }) => {
-	return (
-		<>
-			<Head>
-				<title>{pageTitle}</title>
-				<link rel='icon' href='/favicon.ico' />
-				<link rel="stylesheet" href="https://fonts.cdnfonts.com/css/groupe" />
-			</Head>
-			<div className="flex flex-col h-80 justify-between font-content antialiased">
-				<Header />
-				{children}
-				<Footer />
-			</div>
-		</>
-	)
-}
+const Layout: React.FC<LayoutProps> = ({ children, pageTitle = 'Event Espresso' }) => (
+	<>
+		<Head>
+			<title>{pageTitle}</title>
+			<link rel='icon' href='/favicon.ico' />
+			<link rel="stylesheet" href="https://fonts.cdnfonts.com/css/groupe" />
+		</Head>
+		<body className="flex flex-col h-80 justify-between font-content antialiased">
+			<Header />
+			{children}
+			<Footer />
+		</body>
+	</>
+);
 
 export default Layout
