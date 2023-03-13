a8f0bf039f01060bbf56f5345e8ab297d0c606a4
tn3rb <brent@eventespresso.com>
2023-03-13 12:31:12 -0700

    make content optional

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,7 @@ export const Layout: React.FC<LayoutProps> = ({ children, pageTitle = 'Event Esp
 			<link rel='icon' href='/favicon.ico' />
 		</Head>
 		<body className="flex flex-col h-80 justify-between font-content antialiased">
-			<Header blok={story.content} />
+			<Header blok={story?.content} />
 			{children}
 			<Footer />
 		</body>
