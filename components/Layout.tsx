9970040d63170d142de34e5058eab247e9e28b7f
tn3rb <brent@eventespresso.com>
2023-03-13 12:56:42 -0700

    add StoryblokBridge bridge script

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children, pageTitle = 'Event Espresso',
 			<link rel='icon' href='/favicon.ico' />
 		</Head>
 		<body className="flex flex-col h-80 justify-between font-content antialiased">
+			<script src="//app.storyblok.com/f/storyblok-v2-latest.js" type="text/javascript" />
 			<Header blok={story?.content} />
 			{children}
 			<Footer />
